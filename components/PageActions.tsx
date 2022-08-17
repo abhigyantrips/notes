ce99beef8a8a3cf1c94b6ce999a54b2f806eabe7
Abhigyan Trips <abhigyantrips@gmail.com>
2022-08-17 11:39:29 +0530

    fix: css padding, unused actions.

--- components/PageActions.tsx
+++ components/PageActions.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { IoHeartOutline } from '@react-icons/all-files/io5/IoHeartOutline'
 import { AiOutlineRetweet } from '@react-icons/all-files/ai/AiOutlineRetweet'
 
 import styles from './styles.module.css'
@@ -10,16 +9,6 @@ import styles from './styles.module.css'
 export const PageActions: React.FC<{ tweet: string }> = ({ tweet }) => {
   return (
     <div className={styles.pageActions}>
-      <a
-        className={styles.likeTweet}
-        href={`https://twitter.com/intent/like?tweet_id=${tweet}`}
-        target='_blank'
-        rel='noopener noreferrer'
-        title='Like this post on Twitter'
-      >
-        <IoHeartOutline />
-      </a>
-
       <a
         className={styles.retweet}
         href={`https://twitter.com/intent/retweet?tweet_id=${tweet}`}
