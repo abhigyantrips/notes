4e6ebcfc3f4adcbdd8c66b137877f2a59ebd7388
Abhigyan Trips <abhigyantrips@gmail.com>
2022-09-15 12:23:14 +0530

    feat: add support for katex css and preview.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
-// global styles shared across the entire site
 import 'styles/global.css'
-
-// core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
-
-// global style overrides for notion
 import 'styles/notion.css'
-
-// global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
+import 'katex/dist/katex.min.css'
 
 import * as React from 'react'
 import type { AppProps } from 'next/app'
