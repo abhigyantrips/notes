0c813cc2098ec63202232be78e2c9f25f27a6dde
Abhigyan Trips <abhigyantrips@gmail.com>
2022-12-01 16:13:31 +0530

    fix: remove socials beside content

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -31,7 +31,6 @@ import * as config from 'lib/config'
 import { Loading } from './Loading'
 import { Page404 } from './Page404'
 import { PageHead } from './PageHead'
-import { PageAside } from './PageAside'
 import { Footer } from './Footer'
 import { NotionPageHeader } from './NotionPageHeader'
 
@@ -205,13 +204,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
   const showTableOfContents = !!isBlogPost
   const minTableOfContentsItems = 3
 
-  const pageAside = React.useMemo(
-    () => (
-      <PageAside block={block} recordMap={recordMap} isBlogPost={isBlogPost} />
-    ),
-    [block, recordMap, isBlogPost]
-  )
-
   const footer = React.useMemo(() => <Footer />, [])
 
   if (router.isFallback) {
@@ -293,7 +285,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
         mapPageUrl={siteMapPageUrl}
         mapImageUrl={mapImageUrl}
         searchNotion={config.isSearchEnabled ? searchNotion : null}
-        pageAside={pageAside}
         footer={footer}
         pageTitle={tagsPage && propertyToFilterName ? title : undefined}
       />
