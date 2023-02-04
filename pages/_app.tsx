13e77faf33a081ad1a7712274c492cb1986f5f74
Abhigyan Trips <abhigyantrips@gmail.com>
2023-02-04 18:24:53 +0530

    feat!: center all equations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import 'styles/global.css'
 import 'react-notion-x/src/styles.css'
 import 'styles/notion.css'
 import 'styles/prism-theme.css'
 import 'katex/dist/katex.min.css'
+import 'styles/global.css'
 
 import * as React from 'react'
 import type { AppProps } from 'next/app'
